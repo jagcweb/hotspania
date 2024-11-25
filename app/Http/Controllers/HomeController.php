4296b42371a409b97e23f5df77f9680ca358bd3f
unknown <jagc.webdev@gmail.com>
2024-11-25 20:22:30 +0100

    changes 25/11

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -33,6 +33,7 @@ class HomeController extends Controller
             ->whereNotNull('completed')
             ->whereNull('banned')
             ->orderBy('created_at', 'desc')
+            ->with('images')
             ->get();
 
         return view('home' , [
